9e6fc83bd427b3d933b75499085797817aee4135
Lucas William Gomes Do Amaral <lucaswilliam133@gmail.com>
2020-08-06 23:20:01 -0300

    Add proffy while server on, OK

--- src/server.js
+++ src/server.js
@@ -8,42 +8,61 @@ let proffys = [
         bio: "Loucura, loucura meus amigos",
         subject: "Matemática",
         cost: 35
-    },
-    {
-        name: "paulo",
-        avatar: "https://avatars1.githubusercontent.com/u/62778691?s=460&u=a8c535cfa9213502698b84d45a8424312fa51a3f&v=4",
-        instagram: "lucaswilliam_g",
-        bio: "Jesus está no Monte das Oliveiras ensinando, quando de repente se levanta e diz: <br>y = 3x² + 2x - 3. <br>Espantado, um de seus discípulos pergunta <br>O que é isso, Mestre?<br>Ao que Jesus responde: Calma, é apenas mais uma parábola...",
-        subject: "Matemática",
-        cost: 35
     }
 ]
 
-
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação Física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
 
 
 const express = require ('express')
 const server = express()
-const nunjucks = require ('nunjucks')
 
-nunjucks.configure ("src/views", {
-    express: server,
-    noCache: true
-})
+
+
+
 
 const pageLanding = function (req, res) {
     return res.render("index.html")
 }
 
 const pageStudy = function (req, res){
-    return res.render("study.html", {proffys})
+    let filter = req.query
+    return res.render("study.html", {proffys, filter, subjects})
 }
 
 const pageGiveClasses = function (req, res) {
+    let data = req.query
+
+    const isNotEmpty = Object.keys(data).length > 0
+
+    if (isNotEmpty) {
+        proffys.push(data)
+        return res.redirect("/study")
+    }   
+
     return res.render("give-classes.html")
 }
 
+const nunjucks = require ('nunjucks')
+nunjucks.configure ("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 server.use(express.static ("public"))
+
 // Config statics archives
 server.get("/", pageLanding)
 server.get("/study", pageStudy)
